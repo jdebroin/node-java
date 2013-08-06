c7d9db0f462069daff78f17b1044f58728d407dd
Jacques de Broin <jdebroin@gmail.com>
2013-08-06 12:52:07 -0400

    add test case for Buffer to BYTE array mapping

--- test/buffer-test.js
+++ test/buffer-test.js
@@ -1,13 +1,26 @@
 
-var java=require("..");
-java.classpath.push(".");
-var bufferTest = java.newInstanceSync("BufferTest");
+var java = require("../testHelpers").java;
 
-var buf1 = bufferTest.getArrayOfBytesSync();
-console.log("out: " + JSON.stringify(buf1));
+var nodeunit = require("nodeunit");
+var util = require("util");
 
-var buf2 = new Buffer([5, 4, 3, 2, 1]);
-bufferTest.setArrayOfBytesSync(buf2);
+//java.classpath.push(".");
+exports['Buffer'] = nodeunit.testCase({
+    setUp: function(callback) {
+        this.bufferTest = java.newInstanceSync("BufferTest");
+        callback();
+    },
 
-var buf3 = bufferTest.getArrayOfBytesSync();
-console.log("out: " + JSON.stringify(buf3));
+    "get-set": function(test) {
+        var buf1 = this.bufferTest.getArrayOfBytesSync();
+        var buf2 = new Buffer([1, 2, 3, 4, 5]);
+        test.deepEqual(buf1.toJSON(), buf2.toJSON());
+
+        var buf3 = new Buffer([5, 4, 3, 2, 1]);
+        this.bufferTest.setArrayOfBytesSync(buf3);
+
+        var buf4 = this.bufferTest.getArrayOfBytesSync();
+        test.deepEqual(buf3.toJSON(), buf4.toJSON());
+        test.done();
+    }
+});
